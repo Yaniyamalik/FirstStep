08d1d5cd89f95b2fb3832f4964dc8e57645a8ddf
Yaniyamalik <140041434+Yaniyamalik@users.noreply.github.com>
2025-03-01 01:29:53 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
 
     try {
       // Send data as an object in the body
-      const response = await axios.post('http://localhost:8000/api/v1/users/login', { username, password, email });
+      const response = await axios.post('https://firststep-backend-project.onrender.com/api/v1/users/login', { username, password, email });
       
       // Set success message (optional)
       alert('Login Successful');
